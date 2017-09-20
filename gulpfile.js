ad7d646fedd8902cfe172a552bc84d4b68c49bdb
Johnson Lai <jlwhoo7@gmail.com>
2017-09-20 16:19:06 +0800

    [CSS] Added normalize.css

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sass = require('gulp-sass');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src('src/scss/*.scss')
+    return gulp.src(['src/scss/*.scss'])
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
@@ -21,4 +21,9 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('css', function() {
+	return gulp.src('node_modules/normalize.css/normalize.css')
+		.pipe(gulp.dest('src/css'));
+})
+
+gulp.task('default', ['serve', 'css']);
