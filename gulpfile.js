f272230e75dfe55f1ac14ab63264534a124c4754
Johnson Lai <jlwhoo7@gmail.com>
2017-09-20 10:33:22 +0800

    [Gulp] Setup gulp task for scss

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+    return gulp.src('src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./src"  
+    });
+
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
